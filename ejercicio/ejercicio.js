21876dcea7246733faf958cc27e7592f974eac7a
nia1921 <usugaestefania665@gmail.com>
2024-10-24 12:05:59 -0500

    ejercicio 3 suma de numeros en un lector

---
+++ ejercicio/ejercicio.js
@@ -0,0 +1,9 @@
+let numeros = [1,2,3,4,5,6,7,8,9,10];
+//definir variable para almacenar los numeros
+let suma = 0;
+// recorer el vcetor para sumar los numeros 
+for(let i = 0 ; i < numeros.length ; i ++ ) {
+    suma = suma + numeros[i]; 
+}
+
+console.log(suma);
